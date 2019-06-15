f405ee540392da66511fe262f8ece87373d14b54
Diego Lopes <diego-chez@hotmail.com>
2019-06-15 19:23:03 -0300

    cria o componente banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default props => {
+  return (
+    <div class="jumbotron jumbotron-fluid" id="banner">
+      <div class="container" />
+    </div>
+  );
+};
