58bdc0ea7ceb3f82d591e6bfadc931af779e7c1c
Diego Lopes <diego-chez@hotmail.com>
2019-06-15 18:27:18 -0300

    cria o componente header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand">Tupinicode</a>
+      </div>
+    </nav>
+  );
+};
